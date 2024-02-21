c997fd1f34091769991551645508945099dbd389
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-02-21 11:28:06 +0600

    Shahid Minar

---
+++ Shahid.js
@@ -0,0 +1,10 @@
+const titile = document.querySelector(".titile");
+const des = document.querySelector(".des");
+
+setInterval(() => {
+  titile.style.display = "block";
+}, 1000);
+
+setInterval(() => {
+  des.style.display = "block";
+}, 1500);
